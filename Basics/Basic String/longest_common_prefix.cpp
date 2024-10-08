e6943463a435ff829eab9dd81037787f62d5cfb7
Anuj Vishwakarma <160238331+anuj-vishwakarma@users.noreply.github.com>
2024-10-08 06:36:55 +0530

    Oct 8 2024 6:36 AM

---
+++ Basics/Basic String/longest_common_prefix.cpp
@@ -0,0 +1,24 @@
+#include<iostream> 
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+string longestCommonPrefix(vector<string>& str){
+    vector<char> chars;
+    sort(str.begin(), str.end());
+    return ans;
+
+
+}
+int main(){
+    int n;
+    vector<string> str;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> words[i];
+    }
+    return 0;
+}
