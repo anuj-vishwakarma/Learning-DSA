7284d0340710308089141c65aba240bc34b98e22
Anuj Vishwakarma <160238331+anuj-vishwakarma@users.noreply.github.com>
2024-10-08 06:30:48 +0530

    Update largest_element.cpp

--- Arrays/Fundamentals/largest_element.cpp
+++ Arrays/Fundamentals/largest_element.cpp
@@ -1,1 +1,25 @@
 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+        // Brute force - sort it and return last element
+
+        int largest = nums[0];
+
+        for(auto i : nums){
+            if (i > largest){
+                largest = i;
+            }
+        }
+        return largest;
+    }
+};
+
+int main(){
+
+    return 0;
+}
